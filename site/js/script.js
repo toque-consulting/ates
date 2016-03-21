0564a1a22608f482b6c13bc0d41a72efa4d4089e
petomi <michael.peto10@gmail.com>
2016-03-20 22:10:50 -0400

    first commit - initial files copied from dropbox

---
+++ site/js/script.js
@@ -0,0 +1,233 @@
+function include(scriptUrl) {
+    document.write('<script src="' + scriptUrl + '"></script>');
+}
+
+function isIE() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+};
+
+/* cookie.JS
+ ========================================================*/
+include('js/jquery.cookie.js');
+
+/* Easing library
+ ========================================================*/
+include('js/jquery.easing.1.3.js');
+
+/* PointerEvents
+ ========================================================*/
+;(function ($) {
+    if(isIE() && isIE() < 11){
+        include('js/pointer-events.js');
+        $('html').addClass('lt-ie11');
+        $(function(){
+            PointerEventsPolyfill.initialize({});
+        });
+    }
+})(jQuery);
+
+/* Stick up menus
+ ========================================================*/
+;(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/tmstickup.js');
+
+        $(function () {
+            $('#stuck_container').TMStickUp({})
+        });
+    }
+})(jQuery);
+
+/* ToTop
+ ========================================================*/
+;(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/jquery.ui.totop.js');
+
+        $(document).ready(function () {
+            $().UItoTop({
+                easingType: 'easeOutQuart',
+                containerClass: 'toTop fa fa-angle-up'
+            });
+        });
+    }
+})(jQuery);
+
+/* EqualHeights
+ ========================================================*/
+;(function ($) {
+    var o = $('[data-equal-group]');
+    if (o.length > 0) {
+        include('js/jquery.equalheights.js');
+    }
+})(jQuery); 
+
+/* Copyright Year
+ ========================================================*/
+;(function ($) {
+    var currentYear = (new Date).getFullYear();
+    $(function () {
+        $("#copyright-year").text((new Date).getFullYear());
+    });
+})(jQuery);
+
+
+/* Superfish menus
+ ========================================================*/
+;(function ($) {
+    include('js/superfish.js');
+})(jQuery);
+
+/* Navbar
+ ========================================================*/
+;(function ($) {
+    include('js/jquery.rd-navbar.js');
+})(jQuery);
+
+/* Camera
+========================================================*/
+;(function ($) {
+var o = $('#camera');
+    if (o.length > 0) {
+        if (!(isIE() && (isIE() > 9))) {
+            include('js/jquery.mobile.customized.min.js');
+        }
+
+        include('js/camera.js');
+
+        $(document).ready(function () {
+            o.camera({
+                autoAdvance: true,
+                height: '65.1181%',
+                minHeight: '300px',
+                pagination: false,
+                thumbnails: false,
+                playPause: false,
+                hover: false,
+                loader: 'none',
+                navigation: true,
+                navigationHover: false,
+                mobileNavHover: false,
+                fx: 'simpleFade'
+            })
+        });
+    }
+})(jQuery);
+
+/* Owl Carousel
+========================================================*/
+;(function ($) {
+    var o = $('.owl-carousel');
+    if (o.length > 0) {
+        include('js/owl.carousel.min.js');
+        $(document).ready(function () {
+            o.owlCarousel({
+                margin: 30,
+                smartSpeed: 450,
+                loop: true,
+                dots: true,
+                dotsEach: 1,
+                nav: true,
+                navClass: ['owl-prev fa fa-angle-left', 'owl-next fa fa-angle-right'],
+                responsive: {
+                    0: { items: 1 }
+                }
+            });
+        });
+    }
+})(jQuery);
+
+/* TouchTouch Gallery
+========================================================*/
+;(function ($) {
+    var o = $('.thumb');
+    if (o.length > 0) {
+        include('js/jquery.touch-touch.js');
+        $(document).ready(function () {
+            o.touchTouch();
+        }); 
+    } 
+})(jQuery);
+
+/* Parallax
+=============================================*/
+;(function ($) {
+    include('js/jquery.rd-parallax.js');
+})(jQuery); 
+
+
+/* Smoothscroll 
+ =============================================*/
+;(function ($) {
+    if ($("html").hasClass("smoothscroll")) {
+        include('js/rd-smoothscroll.js');
+    }
+})(jQuery);
+
+/* WOW
+ ========================================================*/
+;(function ($) {
+    var o = $('html');
+
+    if ((navigator.userAgent.toLowerCase().indexOf('msie') == -1 ) || (isIE() && isIE() > 9)) {
+        if (o.hasClass('desktop')) {
+            include('js/wow.js');
+
+            $(document).ready(function () {
+                new WOW().init();
+            });
+        }
+    }
+})(jQuery);
+
+/* Mailform
+=============================================*/
+;(function ($) {
+    include('js/mailform/jquery.form.min.js'); 
+    include('js/mailform/jquery.rd-mailform.min.js');
+})(jQuery);
+
+
+/* Orientation tablet fix
+ ========================================================*/
+;$(function () {
+    // IPad/IPhone
+    var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+        ua = navigator.userAgent,
+
+        gestureStart = function () {
+            viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";
+        },
+
+        scaleFix = function () {
+            if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+                viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+                document.addEventListener("gesturestart", gestureStart, false);
+            }
+        };
+
+    scaleFix();
+    // Menu Android
+    if (window.orientation != undefined) {
+        var regM = /ipod|ipad|iphone/gi,
+            result = ua.match(regM);
+        if (!result) {
+            $('.sf-menus li').each(function () {
+                if ($(">ul", this)[0]) {
+                    $(">a", this).toggle(
+                        function () {
+                            return false;
+                        },
+                        function () {
+                            window.location.href = $(this).attr("href");
+                        }
+                    );
+                }
+            })
+        }
+    }
+});
+
